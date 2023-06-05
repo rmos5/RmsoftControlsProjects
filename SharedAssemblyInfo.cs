c1ed968496129bff53004b0f8de75ac6f7591e8f
Raivo Mosona <raivo.mosona@gmail.com>
2023-06-05 18:21:07 +0300

    SharedAssemblyInfo.cs location updates

--- SharedAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Windows.Markup;
-
-[assembly: AssemblyCompany("rmsoft")]
-[assembly: AssemblyCopyright("©2019-2023 rmsoft")]
-[assembly: AssemblyTrademark("rmsoft™")]
-
-[assembly: XmlnsPrefix("http://schemas.rmsoft.fi/controls", "rmcontrols")]
